4b6905828f40b5dbf746a75d41117589c769608b
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-09-12 00:05:32 -0400

    first commit

---
+++ devices-page.js
@@ -0,0 +1,11 @@
+import { Selector, t } from "testcafe";
+
+const name = Selector(".device-name");
+const type = Selector(".device-type");
+const capacity = Selector(".device-capacity");
+const editButton = Selector(".device-edit");
+const removeButton = Selector(".device-remove");
+
+class Devices {
+  constructor(text) {}
+}
---
+++ test-one.js
@@ -0,0 +1,3 @@
+import devicesPage from "./devices-page";
+
+fixture`ninja-rmm-tests`.page`http://localhost:3001`;
